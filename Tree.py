41e574b0e396bd2bc4791daf163358885c51abe1
shashwatyada <55271160+shashwatyada@users.noreply.github.com>
2026-07-19 21:22:17 +0530

    Add Tree implementation and traversals, count nodes,search node, maximum value

--- Tree.py
+++ Tree.py
@@ -63,6 +63,41 @@ def level_order(root):
         if node.right:
             queue.append(node.right)
 
+# Height of the tree
+def height(root):
+    if root is None:
+        return 0
+    left = height(root.left)
+    right = height(root.right)
+
+    return max(left, right) +1    
+
+# Count Total Nodes
+def count_nodes(root):
+    if root is None:
+        return 0
+    return 1 + count_nodes(root.left) + count_nodes(root.right)
+
+# Search Element 
+def search(root, key):
+    if root is None:
+        return False
+    
+    if root.data == key:
+        return True
+    
+    return search(root.left, key) or search(root.right, key)
+
+# Maximum Value 
+def find_max(root):
+    if root is None:
+        return 0
+    return max(root.data,
+               find_max(root.left),
+               find_max(root.right))
+
+
+
 print("Root : ", root.data)            # Parent
 print("Left Child : ", root.left.data)       # Child
 print("Right Child : ", root.right.data)
@@ -86,3 +121,15 @@ print("\n")
 
 print("\nLevel Order Traversal")
 level_order(root)
+print("\n")
+
+print("Height : ", height(root))
+print("\n")
+
+print("Total Nodes : ", count_nodes(root))
+print("\n")
+
+print("Search 50 : ", search(root,50))
+print("\n")
+
+print("Maximum :", find_max(root))
